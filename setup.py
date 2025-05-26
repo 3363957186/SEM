9f20c335ddc67d7832b09c546f61624ee7b0afa0
Varuna Jasodanand <varunaja@bu.edu>
2025-05-26 11:32:29 -0400

    modify setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
 import setuptools
+import yaml
 
 # read the contents of requirements.txt
-with open('./requirements.txt') as f:
-    requirements = f.read().splitlines()
+# with open('./requirements.txt') as f:
+#     requirements = f.read().splitlines()
+
+def get_requirements_from_env_yml():
+    with open('./environment.yml', 'r') as f:
+        env_data = yaml.safe_load(f)
+    
+    requirements = []
+    
+    # Get pip dependencies if they exist
+    dependencies = env_data.get('dependencies', [])
+    for dep in dependencies:
+        if isinstance(dep, dict) and 'pip' in dep:
+            requirements.extend(dep['pip'])
+    
+    return requirements
+
+requirements = get_requirements_from_env_yml()
 
 setuptools.setup(
     name = 'adrd',
